2433b33d991c62bf52a67e41b1cf091c63441c4a
phoomss <narongsak5az@gmail.com>
2025-04-27 22:24:37 +0700

    .

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,16 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import daisyui from 'daisyui';
 
-const config = {
-  plugins: [tailwindcss, autoprefixer],
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"], // กำหนดธีมที่นี่
+  },
 };
-
-export default config;
